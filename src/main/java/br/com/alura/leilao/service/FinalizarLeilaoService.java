4af3b641a2b5e004ea378c0e9f5c88eaf9a3620e
gabriel <gabriel.romano.3194@hotmail.com>
2022-05-16 16:56:52 -0300

    comportamentos

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,12 @@ import br.com.alura.leilao.model.Leilao;
 @Service
 public class FinalizarLeilaoService {
 
-	@Autowired
 	private LeilaoDao leiloes;
+	
+	@Autowired
+	public FinalizarLeilaoService(LeilaoDao leiloes) {
+		this.leiloes = leiloes;
+	}
 
 	public void finalizarLeiloesExpirados() {
 		List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
@@ -26,7 +31,7 @@ public class FinalizarLeilaoService {
 	}
 
 	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
-		List<Lance> lancesDoLeilao = leilao.getLances();
+		List<Lance> lancesDoLeilao = new ArrayList<>(leilao.getLances());
 		lancesDoLeilao.sort((lance1, lance2) -> {
 			return lance2.getValor().compareTo(lance1.getValor());
 		});
